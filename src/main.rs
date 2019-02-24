686ebcaa05eb942a2a47e149e60450f342aa48bc
Jakub Westfalewski <jakub.westfalewski@allegro.pl>
2019-02-24 22:09:20 +0100

    little refactoring

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,16 @@ struct TraningUrl {
     title: Option<String>,
 }
 
+impl TraningUrl {
+  fn get_tokens(&self) -> String {
+    let title = self.title.clone().unwrap_or(" ".to_string());
+    let mut input = self.url.clone();
+    input.push_str(" ");
+    input.push_str(&title);
+    return tokenize(input);
+  }
+}
+
 fn read_training_examples() -> Result<Vec<TraningUrl>> {
     let mut file = File::open("traning-dataset.json").expect("file not found");
     let mut contents = String::new();
@@ -41,17 +51,10 @@ fn tokenize(input: String) -> String {
 
 fn train() -> NaiveBayes {
     let mut nb = NaiveBayes::new();
-
     let mut documents: HashMap<String, String> = HashMap::new();
 
-    let examples = read_training_examples().unwrap();
-    for example in examples {
-
-      let title = example.title.unwrap_or(" ".to_string());
-      let mut input = example.url;
-      input.push_str(&" ".to_string());
-      input.push_str(&title);
-      let tokens = tokenize(input);
+    for example in read_training_examples().unwrap() {
+      let tokens = example.get_tokens();
 
       if !documents.contains_key(&example.category) {
         documents.insert(example.category.to_owned(), "".to_owned());
